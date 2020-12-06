2f4002e48c4f453222d614869ebfcc5efea4c7d8
yg-0103 <rladusrn89@gmail.com>
2020-12-07 08:38:45 +0900

    버블솔트 수정

--- javascript/algorithm/bubleSort.js
+++ javascript/algorithm/bubleSort.js
@@ -1,5 +1,5 @@
 function bubbleSort(array) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] > array[j]) {
         let arr = array[i];
