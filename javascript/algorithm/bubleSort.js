24cf655661911c3a1c95d21202070a6a66c4e92d
yg-0103 <rladusrn89@gmail.com>
2020-12-06 19:21:08 +0900

    this 와 sort algorithm

---
+++ javascript/algorithm/bubleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let arr = array[i];
+        array[i] = array[j];
+        array[j] = arr;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
+console.log(bubbleSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
